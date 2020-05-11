b0898e026cc8615f9fe4e262ec4542e16275d297
Pelanyo Kamara <pkamara@pkamara.me>
2020-05-11 21:14:47 +0100

    luaxt support

--- src/lj_def.h
+++ src/lj_def.h
@@ -8,24 +8,9 @@
 
 #include "lua.h"
 
-#if defined(_MSC_VER)
 /* MSVC is stuck in the last century and doesn't have C99's stdint.h. */
-typedef __int8 int8_t;
-typedef __int16 int16_t;
-typedef __int32 int32_t;
-typedef __int64 int64_t;
-typedef unsigned __int8 uint8_t;
-typedef unsigned __int16 uint16_t;
-typedef unsigned __int32 uint32_t;
-typedef unsigned __int64 uint64_t;
-#ifdef _WIN64
-typedef __int64 intptr_t;
-typedef unsigned __int64 uintptr_t;
-#else
-typedef __int32 intptr_t;
-typedef unsigned __int32 uintptr_t;
-#endif
-#elif defined(__symbian__)
+/* 11-05-2020: The above is no longer true.*/
+#if defined(__symbian__)
 /* Cough. */
 typedef signed char int8_t;
 typedef short int int16_t;
